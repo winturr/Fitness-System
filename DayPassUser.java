96bb8ac10c2b6db8c50f3d9f10c8f167424b7bb6
winturr <leanmangulabnan95@gmail.com>
2024-11-10 13:27:20 +0800

    Create DayPassUser.java

---
+++ DayPassUser.java
@@ -0,0 +1,12 @@
+package fitness;
+
+public class DayPassUser extends Visitor {
+	private String visitDate;
+	
+	public String getVisitDate() {
+		return visitDate;
+	}
+	public void setVisitDate(String visitDate) {
+		this.visitDate = visitDate;
+	}
+}
