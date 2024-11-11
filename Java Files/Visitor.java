0ab3348246ff0aff1f4ba8fd026462a82f0813c2
Dr3-dev <delfinandre.tp490@gmail.com>
2024-11-11 17:13:00 +0800

    Update Visitor.java
    2 main methods were added with provided documentation.

--- Java Files/Visitor.java
+++ Java Files/Visitor.java
@@ -23,4 +23,36 @@ public class Visitor {
 	public void setVisitorType(String visitorType) {
 		this.visitorType = visitorType;
 	}
+
+	// "*" as a delimiter
+	//Writes and save to visitor text file
+	public void saveToFile() {
+		try(BufferedWriter writer = new BufferedWriter(new FileWriter("Visitor.txt", true))) {
+			writer.write(visitorID + "*" + name + "*" + visitorType + "*");
+			writer.newLine();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//List<Visitor> as a return data type in this method
+	/*List is an interface, concrete class ArrayList implements the List interface
+	 which allows flexibility*/
+	public static List<Visitor> getFromFile() {
+		List<Visitor> visitors = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("Visitor.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split("\\*");
+                String id = data[0];
+                String name = data[1];
+                String type = data[2];
+                visitors.add(new Visitor(id, name, type));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return visitors;
+
 }
