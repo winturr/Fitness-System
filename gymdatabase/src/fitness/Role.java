0889a5783784b3545909a8650e54d02e47cdcca3
winturr <leanmangulabnan95@gmail.com>
2024-11-15 20:41:30 +0800

    Update Role.java

--- gymdatabase/src/fitness/Role.java
+++ gymdatabase/src/fitness/Role.java
@@ -1,4 +1,4 @@
-package fitness
+package fitness;
 
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
@@ -10,86 +10,60 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Role {
-    private String roleID = "";
-    private String roleName = "";
+     private String roleID = "";
+     private String roleName = "";
 
-    public static Scanner sc = new Scanner(System.in);
+     public static Scanner sc = new Scanner(System.in);
 
-    public Role(String roleID, String roleName) {
-        this.roleID = roleID;
-        this.roleName = roleName;
-    }
-    public Role() {
+     public Role(String roleID, String roleName) {
+         this.roleID = roleID;
+         this.roleName = roleName;
+     }
+     public String getRoleId() {
+         return roleID;
+     }
 
-    }
-
-    public String getRoleId() {
-        return roleID;
-    }
-
-    public String getRoleName() {
-        return roleName;
-    }
-
-    public void setRoleId(String roleID) {
-    	this.roleID = roleID;
-    }
-
-    public void saveToFile() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Role.txt", true))) {
-            writer.write(getRoleId() + "*" + getRoleName()+ "*");
-            writer.newLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+     public String getRoleName() {
+         return roleName;
+     }
 
 
-    public static List<Role> getFromFile() {
-        List<Role> roles = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader("Role.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] data = line.split("\\*");
-                String id = data[0];
-                String roleName = data[1];
-                roles.add(new Role(id, roleName));
+     public void saveToFile() {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("Role.txt", true))) {
+                writer.write(getRoleId() + "" + getRoleName()+ "");
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return roles;
-    }
 
 
-    public void add() {
-        
-        
-        boolean isValid = false;
-        List<Role> roles = Role.getFromFile();
-        do {
-    	    System.out.print("Enter new Role ID: ");
-    	    String roleId = sc.nextLine();
-    	    isValid = true;
-    	    for(Role role: roles) {
-    	        if(role.getRoleId().equals(roleId)) {
-    	            System.out.println("Role ID already exists! Please enter a new one.");
-    	            isValid = false; 
-    	            break; 
-    	        }
-    	    }
-    	    if (isValid) {
-    	        setRoleId(roleId);
-    	    }
-    	} while(!isValid);
-        
-        
-        System.out.println("Enter Role name: ");
-        String roleName = sc.nextLine();
+     public static List<Role> getFromFile() {
+            List<Role> roles = new ArrayList<>();
+            try (BufferedReader reader = new BufferedReader(new FileReader("Role.txt"))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] data = line.split("\\*");
+                    String id = data[0];
+                    String roleName = data[1];
+                    roles.add(new Role(id, roleName));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return roles;
+        }
 
-        Role r = new Role(roleID, roleName);
-        r.saveToFile();
 
-    }
+     public static void add() {
+         System.out.println("Enter Role id: ");
+         String roleId = sc.nextLine();
+         System.out.println("Enter Role name: ");
+         String roleName = sc.nextLine();
+
+         Role r = new Role(roleId, roleName);
+         r.saveToFile();
+
+     }
 
 }
