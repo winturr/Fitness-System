0bfeec829bea1391b6e16f72dd3bc919878e6269
KURTYU1OP <adrianokurtiandencing@gmail.com>
2024-11-15 09:17:50 +0800

    Main.java
    Update

--- gymdatabase/src/fitness/Main.java
+++ gymdatabase/src/fitness/Main.java
@@ -1,10 +1,42 @@
 package fitness;
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Welcome to the Fitness Management System!");
+            System.out.println("Please choose an option:");
+            System.out.println("1. Manage Staff");
+            System.out.println("2. Manage Classes");
+            System.out.println("3. Exit");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // Consume newline
+            
+            switch (choice) {
+                case 1:
+                    // Manage Staff
+                    Staff.manageStaffFromUserInput();
+                    break;
+                    
+                case 2:
+                    // Manage Classes
+                    Class.manageClassFromUserInput();
+                    break;
+                    
+                case 3:
+                    // Exit
+                    System.out.println("Exiting the system. Goodbye!");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                    
+                default:
+                    System.out.println("Invalid choice, please try again.");
+                    break;
+            }
+        }
+    }
 }
