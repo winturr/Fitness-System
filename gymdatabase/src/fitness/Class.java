e0e3bf91acebbbb23af978e385f9ac7a1201be3f
KURTYU1OP <adrianokurtiandencing@gmail.com>
2024-11-16 23:41:10 +0800

    Class.java

--- gymdatabase/src/fitness/Class.java
+++ gymdatabase/src/fitness/Class.java
@@ -84,24 +84,16 @@ public class Class {
      
     public static void display() {
         List <Class> class1 = getFromFile();
-        System.out.println(String.format("%s", "--------------------------------------------------------------------------------------------"));
-        System.out.println(String.format("%9s %3s %15s %5s %5s %1s %7s %4s %3s %7s %5s","Class ID" ,"|","Class Name","|","Max Capacity","|","Start Time","|","End Time","|","Staff ID"));
-        System.out.println(String.format("%s", "--------------------------------------------------------------------------------------------"));
+        System.out.println(String.format("%s", "------------------------------------------------------------------------------------------------------------------------------------------------------"));
+        System.out.println(String.format("%9s %3s %26s %5s %15s %10s %15s %9s %3s %26s %5s","Class ID" ,"|","Class Name","|","Max Capacity","|","Start Time","|","End Time","|","Staff ID"));
+        System.out.println(String.format("%s", "------------------------------------------------------------------------------------------------------------------------------------------------------"));
         for(Class c: class1) {
-            System.out.format("%9s %3s %15s %5s %5s %8s %7s %7s %5s %8s %5s", c.getClassID(), "|", c.getClassName(),"|", c.getMaxCapacity(),"|", c.getStartTime(),"|", c.getEndTime(),"|",c.getStaffID(),"|");
+            System.out.format("%9s %3s %26s %5s %15s %10s %15s %9s %3s %26s %5s", c.getClassID(), "|", c.getClassName(),"|", c.getMaxCapacity(),"|", c.getStartTime(),"|", c.getEndTime(),"|",c.getStaffID(),"|");
             System.out.println();
         }
     }
 
-    public static boolean isClassIDValid(String classID) {
-            List<Class> class1 = getFromFile();
-            for (Class clas : class1) {
-                if (clas.getStaffID().equals(classID)) {
-                    return true;
-                }
-            }
-            return false;
-        }
+    
     
     
     public static boolean isStaffIDValid(String staffID) {
@@ -236,34 +228,47 @@ public class Class {
 
     
     
-    
+    public static boolean isClassIDValid (String classID) throws IOException {
+		 List<ClassRegistration> classGet = ClassRegistration.getFromFile();
+	 		for (ClassRegistration class1 : classGet) {
+	 			if (class1.getClassID().equals(classID)) {
+	 				return true;
+	 			}
+	 		}
+	 	return false;
+	 }
     
     public static void delete() throws IOException {
     List<Class> classList = getFromFile();
-    System.out.print("Enter Class ID to delete: ");
-    String id = inp.nextLine();
-    if(ClassRegistration.isClassIDValid(id)){
-        System.out.println("Class is Used by Class Regidtration file, invalid delete");
-    }
-    else{
-        System.out.print("Are you sure you want to delete Class ID " + id + "? [1] Yes [2] No: ");
-        String deleteConfirm = inp.nextLine();
+    int ctr =1;
+    while(ctr==1){
+        System.out.print("Enter Class ID to delete: ");
+        String classid = inp.nextLine();
 
-        if (deleteConfirm.equals("1")) {
-
-            classList.removeIf(class1 -> class1.getClassID().equals(id));
-
-
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("Class.txt"))) {
-                for (Class class1 : classList) {
-                    writer.write(class1.getClassID() + "*" + class1.getClassName() + "*" + class1.getMaxCapacity() + "*" + class1.getStartTime() + "*" + class1.getEndTime() + "*" + class1.getStaffID());
-                    writer.newLine();
-                }
-                System.out.println("Class deleted successfully.");
-            }
-        } else {
-            System.out.println("Deletion canceled.");
+        if(isClassIDValid(classid)){
+            System.out.println("Class is Used by Class Registration file, invalid delete");
         }
+        else{
+            System.out.print("Are you sure you want to delete Class ID " + classid + "? [1] Yes [2] No: ");
+            String deleteConfirm = inp.nextLine();
+
+            if (deleteConfirm.equals("1")) {
+
+                classList.removeIf(class1 -> class1.getClassID().equals(classid));
+
+
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("Class.txt"))) {
+                    for (Class class1 : classList) {
+                        writer.write(class1.getClassID() + "*" + class1.getClassName() + "*" + class1.getMaxCapacity() + "*" + class1.getStartTime() + "*" + class1.getEndTime() + "*" + class1.getStaffID());
+                        writer.newLine();
+                    }
+                    System.out.println("Class deleted successfully.");
+                    ctr = 0;
+                }
+            } else {
+                System.out.println("Deletion canceled.");
+            }
+    }
 }
 }
 
