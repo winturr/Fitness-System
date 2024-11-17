a941be57e6065b1445996ecdc04f4ca95127eda4
Renzzio <diazrenz30@gmail.com>
2024-11-17 18:33:21 +0800

    Update Member.java

--- gymdatabase/src/fitness/Member.java
+++ gymdatabase/src/fitness/Member.java
@@ -73,8 +73,8 @@ public class Member {
 	//scans all available records in Member.txt
 	public static List<Member> getFromFile() throws IOException{
 		List<Member> m = new ArrayList<>();
-		BufferedReader reader = new BufferedReader(new FileReader("Member.txt"));
-            String line;
+		try (BufferedReader reader = new BufferedReader(new FileReader("Member.txt"))) {
+			String line;
             while ((line = reader.readLine()) != null) {
                 String[] data = line.split("\\*");
                 String id = data[0];
@@ -85,7 +85,8 @@ public class Member {
                 String vID = data[5];
                 m.add(new Member(id,date,date2,contact,status,vID));
             }
-        return m;
+		}
+            return m;
 	}
 	public static void add()throws IOException {
 		LocalDate date = LocalDate.now();
@@ -97,20 +98,22 @@ public class Member {
         boolean isValid = false;
         int ctr = 0;
         do {
-        	System.out.print("Enter Member ID: ");
+        	System.out.print("Enter new Member ID: ");
     		memID = i.nextLine();
      		for (Member member : memberList) {
-     			if(member.getMemberID().equals(memID)) {	
+     			if(member.getMemberID().equals(memID) || memID.isEmpty()) {	
+     				if(member.getMemberID().equals(memID)) {    					
+         				System.out.println("Member ID already exists. Please try again.");
+     				} else if (memID.isEmpty()) {
+     					System.out.println("Member ID cannot be empty.");
+     				}
      				ctr++;
-     				System.out.println("Member ID already exists. Please try again.");
+     				break;
      			}
      		}
      		if (ctr == 0) {
      			isValid = true;
      		}
-     		if (isEmpty(memID)) {
-				isValid = false;
-			}
      		ctr = 0;
         }
         while(isValid == false);
@@ -131,17 +134,18 @@ public class Member {
 					}
 					if (ctr != 0) {
 						isValid = false;
+						System.out.println("Invalid Visitor ID. Please try again");
 					}
 					else {
 						Member m = new Member(memID, date.format(formatter), futureDate.format(formatter), contactNo, "Active", visitID);
 						m.saveToFile();
-						System.out.println("Saved to \"Member.txt\".");
+						System.out.println("Member added Successfully.");
 					}
 					ctr = 0;
 				}
 			}
 		}
-		while(isValid == false);
+		while(!isValid);
 	}
 	//displays Member.txt in table form
 	//static method to directly access method from main without instantiation
@@ -153,6 +157,7 @@ public class Member {
 		for(Member me: members) {
 			System.out.println(String.format("%9s %3s %5s %10s %5s %8s %11s %5s %10s %4s %5s", me.getMemberID(),"|",me.getMembershipStartDate() ,"|", me.getMembershipEndDate(),"|",me.getContactNo(),"|",me.getStatus(), "|", me.getVisitorID()));
 		}
+		System.out.println(String.format("%s", "---------------------------------------------------------------------------------------------------"));
 	}
 	public static void update()throws IOException {
 		List<Member> memberList = Member.getFromFile();//.txt file into ArrayList
@@ -160,38 +165,64 @@ public class Member {
 		int ctr = 0;
 		String id;
 		do {
-			System.out.print("Look for ID: ");
+			System.out.print("Look for Member ID: ");
 			id = i.nextLine();
 			for (Member member: memberList) {
 				if(member.getMemberID().equals(id)) {
 					ctr++;
-					System.out.println("Member ID "+ member.getMemberID() +" Found.");
 				}
 			}
 			if (ctr != 0) {
 				isValid = true;
 			}
 			else {
-				System.out.println("Member ID not found.");
+				System.out.println("Member ID not found. Please try again.");
 			}
 		}
 		while(isValid == false);
-		System.out.print("Enter new Contact Info: ");
-		String nc = i.nextLine();
-		System.out.print("Enter new status, [1]Active [2]Inactive: ");
-		String ns = i.nextLine();
-		switch (ns) {
-		case "1":
-			ns = "Active";
-			break;
-		case "2":
-			ns = "Inactive";
-		}
+		
+		
 	    for (Member member : memberList) {//looks for given MemberID
 	        if (member.getMemberID().equals(id)) {//if found
 	        	//replaces values
-	            member.setContactNo(nc);
-	            member.setStatus(ns);
+	        	LocalDate date = LocalDate.now();
+	    		LocalDate futureDate = date.plusYears(3);
+	            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	            String currentDate = date.format(formatter);
+	            String newFutureDate = futureDate.format(formatter);
+	            String renew ="";
+	            boolean isRenewed=false;
+	    		if (member.getStatus().equals("Inactive")) {
+	    			System.out.print("Renew?[1]Yes [2]No: ");
+		    		renew = i.nextLine();
+		    		switch (renew) {
+		    			case "1":
+		    				isRenewed=true;
+		    				member.setMembershipStartDate(currentDate);
+		    				member.setMembershipEndDate(newFutureDate);
+		    				member.setStatus("Active");
+		    				System.out.println("Renewed");
+		    		        break;
+		    				
+		    		}
+	    		}
+	            
+	        	System.out.print("Current Contact Info: " +member.getContactNo()+"\nEnter new Contact Info: ");
+	    		String nc = i.nextLine();
+	    		if (member.getStatus().equals("Active")  && !isRenewed) {
+	    			System.out.print("Current Status: "+member.getStatus()+"\nEnter new status, [1]Active [2]Inactive: ");
+		    		String ns = i.nextLine();
+		    		switch (ns) {
+		    		case "1":
+		    			ns = "Active";
+		    			break;
+		    		case "2":
+		    			ns = "Inactive";
+		    		}
+		    		member.setStatus(ns);
+	    		}
+	    		
+	            member.setContactNo(nc);            
 	            member.saveToFile();
 	            break;
 	        }
@@ -201,22 +232,19 @@ public class Member {
         	writer.write(member.getMemberID() + "*" + member.getMembershipStartDate() + "*" + member.getMembershipEndDate() + "*" + member.getContactNo() + "*" + member.getStatus() + "*" + member.getVisitorID() + "*");
             writer.newLine();
         }
-        System.out.print("Updated Entry.");
+        System.out.println("Updated Entry.");
         writer.close();
 	}
 	public static void delete() throws IOException {
 		List<Member> memberList = Member.getFromFile();//.txt file into ArrayList
-		int ctr = 0;
 		boolean isValid = false;
 		do {
-			System.out.print("Look for ID: ");
+			System.out.print("Look for  Member ID: ");
 			String id = i.nextLine();
 			for (Member member : memberList) {
 				if (member.getMemberID().equals(id)) {
-					ctr++;
 					isValid = true;
-					System.out.println("Member ID " + member.getMemberID() + " Found.");
-					System.out.print("Are you sure you want to delete " + id + "?\n[1]Yes [2]No --> ");
+					System.out.print("Are you sure you want to delete " + id + "?[1]Yes [2]No: ");
 			        String deleteConfirm = i.nextLine();
 			        switch(deleteConfirm) {
 			        	case "1"://case 1 / "YES"
@@ -228,22 +256,19 @@ public class Member {
 			        			}
 			        		}
 			        		writer.close();
-			        		System.out.println("Deleted.");
+			        		System.out.println("Member Deleted Successfully.");
 			        		break;
 			        	case "2": //case 2 / "NO"
-			        		System.out.println("Deletion cancelled.");
+			        		System.out.println("Deletion Canceled.");
 			        		break;
 			        }
+				} 
 				}
+			if (!isValid){
+				System.out.println("Member ID not found. Please try again.");
 			}		
 		}
 		while(!isValid);
 	}
-	public static boolean isEmpty(String input) {
-		if (input != "") {
-			return false;
-		}
-		return true;
-	}
+	
 }
-
