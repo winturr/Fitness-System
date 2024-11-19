29c1b93138a05de55e36382c650fd03b7772fae2
winturr <leanmangulabnan95@gmail.com>
2024-11-19 20:30:04 +0800

    Update CRD.java

--- projectfitness/src/fitness/CRD.java
+++ projectfitness/src/fitness/CRD.java
@@ -2,7 +2,7 @@ package fitness;
 
 import java.io.IOException;
 
-public abstract class CRD { //idk
+public abstract class CRD { //template for all classes with add, display, and delete behaviors.
 	public abstract void saveToFile() throws IOException;
 	public abstract void add() throws IOException;
 	public abstract void display() throws IOException;
